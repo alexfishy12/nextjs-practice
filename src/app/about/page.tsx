7944efe6f042911765532c0fe66f6c5c37eeb616
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2025-04-28 22:32:22 -0400

    Created nextjs app, added simple about and contact page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,14 +5,16 @@ export default function About() {
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div>
             <div className="flex gap-5">
-                <a href="/about" className="underline">About</a>
-                <a href="/contact" className="underline">Contact</a>
+                <a href="/" className="text-gray-400 flex gap-3">
+                    <div>Home</div><div>&gt;</div>
+                </a>
+                <a href="/about">About</a>
             </div>
             <div className="heading1 text-center">
-                Home
+                About
             </div>
             <div className="paragraph1 text-center">
-                This is the homepage of Alex's Next.js project sandbox.
+                This is a project sandbox used to complete a 30-minute per day 30 day challenge to improve my Next.js skills. By the end of the 30 days, I'll be 90% better than people who "tried Next.js once".
             </div>
         </div>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
 
-export default function About() {
+export default function Contact() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div>
             <div className="flex gap-5">
-                <a href="/about" className="underline">About</a>
-                <a href="/contact" className="underline">Contact</a>
+                <a href="/" className="text-gray-400 flex gap-3">
+                    <div>Home</div><div>&gt;</div>
+                </a>
+                <a href="/about">Contact</a>
             </div>
             <div className="heading1 text-center">
-                Home
+                Contact
             </div>
             <div className="paragraph1 text-center">
-                This is the homepage of Alex's Next.js project sandbox.
+                <div>
+                  me@fisherbit.llc
+                </div>
+                <div>
+                  +1 (908) 300-0314
+                </div>
             </div>
         </div>
     </div>
